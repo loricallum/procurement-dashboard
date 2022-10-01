324b6fa56b5a392b21c7f901d6f4f1f5858d2870
Lori <lorimlewis@gmail.com>
2022-10-01 11:13:37 +0100

    Add files via upload

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,26 @@
+
+// import './list.scss';
+// export const List = () => {
+//   return (
+//     <div>List</div>
+//   )
+// }
+
+import "./list.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
+
+const List = () => {
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
+    </div>
+  )
+}
+
+export default List
